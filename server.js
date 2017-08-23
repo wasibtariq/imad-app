f2d9676b7366ca3f772b41030479eb4410cac4ea
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-23 23:40:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ function createTemplate(data){
     
 var htmlTemplate = `<html>
 <head>
-    <title>${title};</title>
+    <title>${title}</title>
     <meta name="viewport" content="width-device-width, initial-scale=1" />
     <link href="/ui/style.css" rel="stylesheet" />
     </head>
@@ -56,13 +56,13 @@ var htmlTemplate = `<html>
         </div>
         <hr/>
         <h3>
-            ${heading};
+            ${heading}
         </h3>
         <div>
-            ${date};
+            ${date}
         </div>
         <div>
-          ${content};
+          ${content}
         </div>
         </div>
     </body>
