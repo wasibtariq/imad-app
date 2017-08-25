abf8388b91a89e6d464aab434591736e4eace992
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 02:21:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ submitbut.onclick= function () {
                 var list='';
                 for(var i=0;i<names.length;i++)
                 {
-                    list+= <li> +names[i]+ </li>;
+                    list+= '<li>' +names[i]+ '</li>';
                 }
                 var ul=document.getElementById('namelist');
                 ul.innerHTML=list;
