62d8025fcc48cd3355cc7c905351aa72c50db80a
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 01:09:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,24 @@
-console.log('Loaded!');
 
-counter=0;
 var button = document.getElementById('counter');
 button.onclick= function(){
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function ()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status == 200)
+            {
+                var counter = request.responseText;
+                counter = counter+1;
+                var span = document.getElementById('count');
+                span.innerHTML= counter.toString();
+            }
+        }
+    }
+    
+    request.open('GET', 'http://wasibtariq95.imad.hasura-app.io/',true);
+    request.send(null);
     
 }
