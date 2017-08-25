85a4ed243e0aeb1193eb80ca52a490d7c5b971cb
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 01:58:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var names = [];
-app.get('/submit-name', function (req, res) {
-    var name= req.query.name;
-    names.push(name);
-  res.send(JSON.stringify(names));
-});
-
 var articles = {
 'article-one' : {
 title: 'Article-one ~ Wasib Tariq',
@@ -86,6 +79,13 @@ var counter=0;
 app.get('/counter', function (req, res) {
      counter = counter + 1;
   res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function (req, res) {
+    var name= req.query.name;
+    names.push(name);
+  res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function (req, res) {
