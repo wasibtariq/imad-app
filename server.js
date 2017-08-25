17ab8f24ff4d512c486f62fe36328a2d74166bd8
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 00:49:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
 
 var counter=0;
 app.get('/counter', function (req, res) {
-    var counter = counter + 1;
+     counter = counter + 1;
   res.send(counter.toString());
 });
 
