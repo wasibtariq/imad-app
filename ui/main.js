e3241d03d668f90cda16a2ec5eecf4d96c122593
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 00:57:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+counter=0;
+var button = document.getElementById('counter');
+button.onclick= function(){
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML= counter.toString();
+    
+}
