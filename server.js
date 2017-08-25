d136ce65137bc0abe900b853a4021b75696c95c8
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 01:16:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var counter=0;
+app.get('/counter', function (req, res) {
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName = req.params.articleName;
