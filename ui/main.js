e0e0d70cc67b4c2b5dbc3d35a344008c4adccc35
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-26 02:09:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,14 +26,30 @@ button.onclick= function(){
 var nameInput = document.getElementById('name');
 var submitbut = document.getElementById('submit_but');
 
-submitbut.onclick = function(){
+submitbut.onclick= function(){
     
-    var names=[name1,name2];
-    var list='';
-    for(var i=0;i<names.length;i++)
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function ()
     {
-        list+= <li> +names[i]+ </li>;
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-}
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                 var names=request.responseText;
+                 names = JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++)
+                {
+                    list+= <li> +names[i]+ </li>;
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    
+    request.open('GET', 'http://wasibtariq95.imad.hasura-app.io/submit-name?name=' +name,true);
+    request.send(null);
+    
+};
