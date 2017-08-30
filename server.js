3fbf2bbd672fe949f71824316c983ff6e963c9d1
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-31 00:31:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user:'wasibtariq95',
     database:'wasibtariq95',
     host:'http://db.imad.hasura-app.io',
-    port:'localhost:5432',
+    port:'5432',
     password: process.env.DB_PASSWORD
 };
 
