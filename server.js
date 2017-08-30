3709d3b96a1684ecfe0a8f96c8c0d375062c0eef
Wasib Tariq <wasibtariq@users.noreply.github.com>
2017-08-31 00:26:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'wasibtariq95,'
+    database:'wasibtariq95',
+    host:'http://db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,6 +82,19 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+ pool.query('SELECT * FROM test', function(err,result){
+    if(err)
+    {
+        res.status(500).send(err.toString());
+    }
+    else{
+        res.send(JSON.stringify(result));
+    }
+ });
 });
 
 var counter=0;
